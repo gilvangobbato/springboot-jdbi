965814fedd74f51ad45dfa50b80d18136f37635e
Gilvan André Gobbato <gilvangobbato@gmail.com>
2020-09-02 18:11:44 -0300

    Criando primeiro objeto e Mapper

--- src/main/java/com/github/gilvangobbato/domain/Paciente.java
+++ src/main/java/com/github/gilvangobbato/domain/Paciente.java
@@ -1,10 +1,20 @@
 package com.github.gilvangobbato.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.beans.ConstructorProperties;
 
 @Data
 public class Paciente {
     private Long id;
     private String nome;
     private String documento;
+
+    @ConstructorProperties({"id", "nome", "documento"})
+    public Paciente(Long id, String nome, String documento) {
+        this.id = id;
+        this.nome = nome;
+        this.documento = documento;
+    }
 }
---
+++ src/main/java/com/github/gilvangobbato/domain/mapper/PacienteMapper.java
@@ -0,0 +1,18 @@
+package com.github.gilvangobbato.domain.mapper;
+
+import com.github.gilvangobbato.domain.Paciente;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PacienteMapper implements RowMapper<Paciente> {
+
+    @Override
+    public Paciente map(ResultSet rs, StatementContext ctx) throws SQLException {
+        return new Paciente(rs.getLong("id"),
+                rs.getString("nome"),
+                rs.getString("documento"));
+    }
+}
